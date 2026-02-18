94b6d5dd4e975b0683158d54937b68212269b723
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2026-02-18 17:28:42 +0700

    cleanup

--- kubejs/startup_scripts/skeleton_merchant.js
+++
@@ -1,9 +0,0 @@
-StartupEvents.registry("minecraft:villager_type", (event) => {
-  const villager = event.create("kubejs:villager_skeleton");
-  villager.displayName("Житель-скелет");
-});
-
-StartupEvents.registry("minecraft:villager_profession", (event) => {
-  const merchant = event.create("kubejs:skeleton_merchant");  
-  merchant.displayName("Скелет-торговец");
-});
