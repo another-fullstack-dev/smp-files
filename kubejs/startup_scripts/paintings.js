7af9d2cccaeeac83bf061c233885da41c432e85d
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2026-02-18 17:27:40 +0700

    add new painting types

---
+++ kubejs/startup_scripts/paintings.js
@@ -0,0 +1,5 @@
+StartupEvents.registry("minecraft:painting_variant", (event)=>{
+    event.create("orokin_hall").width(16).height(32).tag('minecraft:placeable');
+    event.create("orokin_hall_dark").width(16).height(32).tag('minecraft:placeable');
+    event.create("sunset").width(16).height(16).tag('minecraft:placeable');
+})
