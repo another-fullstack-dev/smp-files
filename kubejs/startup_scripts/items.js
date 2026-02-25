948fd8e4c076a7c920455a999900793c44121acd
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2026-02-25 12:14:16 +0700

    add bf sword

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -2,4 +2,10 @@ StartupEvents.registry("item", (event) => {
   event.create("roasted_mushroom").food((food) => {
     food.hunger(2).saturation(1);
   });
+  event
+    .create("bf_sword", "sword")
+    .texture("kubejs:item/bf_sword")
+    .tier("netherite")
+    .attackDamageBaseline(11)
+    .speed(1.4)
 });
