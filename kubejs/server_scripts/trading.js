450ea138dae0300e2012a8072aca98736146d408
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2026-02-25 12:14:54 +0700

    add star merchant trades

--- kubejs/server_scripts/trading.js
+++ kubejs/server_scripts/trading.js
@@ -5,10 +5,26 @@ const betterDungeonsMaps = VillagerUtils.createStructureMapTrade(
     .add(10, "betterdungeons:zombie_dungeon")
     .add(10, "betterdungeons:spider_dungeon")
     .add(10, "betterdungeons:skeleton_dungeon"),
-).displayName(Text.of("Карта подземелья"))
+).displayName(Text.of("Карта подземелья"));
 
 MoreJSEvents.wandererTrades((event) => {
   event.addTrade(1, "5x minecraft:emerald", "fishing101:fish_book");
   event.addTrade(1, "5x minecraft:emerald", "fishing101:meal_book");
   event.addTrade(2, betterDungeonsMaps);
+});
+
+MoreJSEvents.villagerTrades((event) => {
+  const starMerchant = "spacecatcustomprofessions:star_merchant" // nice namespace moron
+  event.addTrade(
+    starMerchant, 
+    1,
+    "minecraft:nether_star",
+    Item.of("minecraft:fishing_rod").enchant("minecraft:lure", 10).withName("Алмазом инкрустированная золотая удочка"),
+  );
+  event.addTrade(
+    starMerchant, 
+    1,
+    "minecraft:nether_star",
+    Item.of("kubejs:bf_sword").withName("Слишком большой меч"),
+  );
 });
