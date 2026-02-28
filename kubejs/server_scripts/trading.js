7a7f0bdd08e45488bdad230bab7a8c7dcf7c54bc
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2026-02-28 11:34:26 +0700

    formatting

--- kubejs/server_scripts/trading.js
+++ kubejs/server_scripts/trading.js
@@ -15,33 +15,46 @@ MoreJSEvents.wandererTrades((event) => {
 
 MoreJSEvents.villagerTrades((event) => {
   const starMerchant = "spacecatcustomprofessions:star_merchant"; // nice namespace moron
-  event.addTrade(
-    starMerchant,
-    1,
-    ["64x minecraft:gold_ingot", "32x minecraft:diamond"],
-    "minecraft:nether_star",
-  ).maxUses(1).priceMultiplier(0.05).villagerExperience(10);
-  event.addTrade(
-    starMerchant,
-    1,
-    "minecraft:nether_star",
-    Item.of("minecraft:fishing_rod")
-      .enchant("minecraft:lure", 10)
-      .withName("Алмазом инкрустированная золотая удочка"),
-  ).maxUses(1).villagerExperience(10);
-  event.addTrade(
-    starMerchant,
-    2,
-    "3x minecraft:nether_star",
-    Item.of("kubejs:bf_sword"),
-  ).maxUses(1).villagerExperience(70);
-  event.addTrade(
-    starMerchant,
-    2,
-    "5x minecraft:nether_star",
-    Item.of("minecraft:netherite_pickaxe")
-      .enchant("minecraft:unbreaking", 5)
-      .enchant("minecraft:fortune", 4)
-      .withName("Экскаватор"),
-  ).maxUses(1).villagerExperience(70);
+  event
+    .addTrade(
+      starMerchant,
+      1,
+      ["64x minecraft:gold_ingot", "32x minecraft:diamond"],
+      "minecraft:nether_star",
+    )
+    .maxUses(1)
+    .priceMultiplier(0.05)
+    .villagerExperience(10);
+  event
+    .addTrade(
+      starMerchant,
+      1,
+      "minecraft:nether_star",
+      Item.of("minecraft:fishing_rod")
+        .enchant("minecraft:lure", 10)
+        .withName("Алмазом инкрустированная золотая удочка"),
+    )
+    .maxUses(1)
+    .villagerExperience(10);
+  event
+    .addTrade(
+      starMerchant,
+      2,
+      "3x minecraft:nether_star",
+      Item.of("kubejs:bf_sword"),
+    )
+    .maxUses(1)
+    .villagerExperience(70);
+  event
+    .addTrade(
+      starMerchant,
+      2,
+      "5x minecraft:nether_star",
+      Item.of("minecraft:netherite_pickaxe")
+        .enchant("minecraft:unbreaking", 5)
+        .enchant("minecraft:fortune", 4)
+        .withName("Экскаватор"),
+    )
+    .maxUses(1)
+    .villagerExperience(70);
 });
