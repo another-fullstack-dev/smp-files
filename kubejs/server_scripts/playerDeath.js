1a841fb52f6e45dc4be1e602e9e93066e0395f33
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2026-03-06 18:30:59 +0700

    implement basic penalty for dying
    we are switching to "keepInventory true". to balance this all tools and
    armor will be damaged by 10% of their durability every time player dies.
    the durability cannot fall bellow 1 tho, meaning you cant destroy equipment
    this way. this is to guard agaist frustration.

---
+++ kubejs/server_scripts/playerDeath.js
@@ -0,0 +1,32 @@
+EntityEvents.death((event) => {
+  if (!event.entity.isPlayer()) return;
+  const player = event.player;
+
+  const inventory = player.inventory.allItems;
+  const gear = inventory.filter((item) => {
+    return isGear(item);
+  });
+  gear.forEach((item) => {
+    const damageBy = item.maxDamage * 0.1;
+    const currentDamage = item.damageValue;
+    let finalValue = currentDamage + damageBy;
+    if (finalValue >= item.maxDamage) {finalValue = item.maxDamage - 1} // dont break it completely just yet...
+    item.setDamageValue(finalValue);
+  });
+});
+
+/**
+ *
+ * @param {Internal.ItemStack} item
+ */
+function isGear(item) {
+  const tags = ["c:armors", "c:tools"];
+  let isValid = false;
+  for (const tag of tags) {
+    if (item.hasTag(tag)) {
+      isValid = true;
+      break;
+    }
+  }
+  return isValid;
+}
