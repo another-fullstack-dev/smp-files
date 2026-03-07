a08c5e5f525dbdb4c13048c45f312e7e326c745f
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2026-03-07 10:08:26 +0700

    drop some random items on death

--- kubejs/server_scripts/playerDeath.js
+++ kubejs/server_scripts/playerDeath.js
@@ -6,15 +6,35 @@ EntityEvents.death((event) => {
   const gear = inventory.filter((item) => {
     return isGear(item);
   });
+
+  inventory.forEach((item) => {
+    let isInGear = gear.find((gearItem) => {
+      return gearItem == item;
+    });
+    if (isInGear) return;
+    
+    const count = item.count;
+    for (let i = 0; i < count; i++) {
+      if (Math.random() < 0.1) {
+        event.level.runCommand(
+          `summon minecraft:item ${player.x} ${player.y} ${player.z} {Item:{id:"${item.id}",Count:1b}}`,
+        ); // drop it nicely on their death location
+        item.shrink(1); // take it from player
+      }
+    }
+  });
+
   gear.forEach((item) => {
     const damageBy = item.maxDamage * 0.1;
     const currentDamage = item.damageValue;
     let finalValue = currentDamage + damageBy;
-    if (finalValue >= item.maxDamage) {finalValue = item.maxDamage - 1} // dont break it completely just yet...
+    if (finalValue >= item.maxDamage) {
+      finalValue = item.maxDamage - 1;
+    } // dont break it completely just yet...
     item.setDamageValue(finalValue);
   });
 
-  player.setXp(player.xp / 2)
+  player.setXp(player.xp / 2);
 });
 
 /**
